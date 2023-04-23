92d78554ea052e96a12292a9d0261141f0690922
Kolon75 <gumbatoff.emil@yandex.ru>
2023-04-23 18:44:41 +0300

    Добавьте файлы проекта.

---
+++ Test2/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.Remoting.Messaging;
+
+namespace Test2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "C:/Users/gumba/OneDrive/Рабочий стол/text.txt";
+
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string text = reader.ReadToEnd();
+                string[] words = text.Split(' ', '.', ',', '!', '?','"','\r',';','\n','-','(',')');
+                int[] count = new int[words.Length];           
+                for (int i = 0; i < words.Length; i++)
+                {
+                    words[i] = words[i].ToLower();
+                    
+                }
+                for (int i = 0; i < words.Length; i++)
+                {
+                    if (words[i] != null && words[i] != "")
+                    {
+
+                        for (int j = i + 1; j < words.Length; j++)
+                        {
+                            if (words[i] == words[j])
+                            {
+                                count[i]++;
+                                words[j] = null;
+                            }
+                        }
+                    }
+
+                }
+                for (int i = 0; i < words.Length; i++)
+                {
+                    for (int j = 0; j < words.Length-1; j++)
+                    {
+                        if (count[j] < count[j + 1])
+                        {
+                            int t = count[j + 1];
+                            count[j + 1] = count[j];
+                            count[j] = t;
+                            string bb = words[j + 1];
+                            words[j + 1] = words[j];
+                            words[j] = bb;
+                        }
+                    }
+                }
+                for (int i = 0; i < words.Length; i++)
+                {
+                    if (words[i] != null && words[i] != "")
+                    {
+                        Console.WriteLine(words[i] + " " + count[i]);
+                    }
+                }
+            }   
+            Console.ReadKey();
+        }
+    }
+}
